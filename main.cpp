3a97ec3a004a48ce129042b48129de98fc08beba
itsmeshanky <mate.szabo1215@gmail.com>
2026-03-02 19:06:21 +0100

    Javitott kod

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //rossz konstans nev
-    std::cout << '1-100 ertekek duplazasa' //rossz idezojel, nincs pontosvesszo
-    for (int i = 0;) //ciklus feltetel hianyzik
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //ciklus feltetele hibas
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //nincs kiirva valtozo es pontosvesszo sincs
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //nincs inicializalva
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] //nincs pontosvesszo
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    //nincs delete
+    delete[] b;
     return 0;
 }

e4885690698b152d35a35cdaff8fa3c34ff0629c
itsmeshanky <mate.szabo1215@gmail.com>
2026-03-02 19:05:23 +0100

    Kikommentelt hibak

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //rossz konstans nev
+    std::cout << '1-100 ertekek duplazasa' //rossz idezojel, nincs pontosvesszo
+    for (int i = 0;) //ciklus feltetel hianyzik
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //ciklus feltetele hibas
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //nincs kiirva valtozo es pontosvesszo sincs
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //nincs inicializalva
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //nincs pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //nincs delete
     return 0;
 }
